a51cc11d0b6ed8fb0b9f4aca719b7e84aa291544
Patricio Sanchez <patriciosanchezrios96@gmail.com>
2023-04-26 02:55:21 +0200

    primer

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './componentes/Navbar';
+import Header from './componentes/Header';
+import Footer from './componentes/Footer';
+function App() {
+  
+  return (
+  
+
+   
+
+    <div >
+    <Header/>
+
+    <Navbar/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+        <i className="fa-brands fa-instagram fa-2xl"></i>
+        <i class="fa-brands fa-facebook fa-2xl"></i>
+        <i class="fa-brands fa-twitter fa-2xl"></i>
+    </div>
+  )
+}
+
+export default Footer
